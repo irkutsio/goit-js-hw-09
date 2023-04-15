c59c96af3446073cf7d53ede622bbc424901c54d
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-15 15:39:39 +0300

    2-task-done

---
+++ src/js/02-timer.js
@@ -0,0 +1,67 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+const btnStartEl = document.querySelector('.timer-btn');
+const countdownEl = document.querySelector('.timer');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+let selectedDate;
+
+btnStartEl.addEventListener('click', onClick)
+btnStartEl.disabled = true;
+
+flatpickr(inputEl, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (Date.now() > selectedDates[0]) {
+      btnStartEl.disabled = true;
+      alert('Please choose a date in the future');
+    } else {
+      btnStartEl.disabled = false;
+      selectedDate = selectedDates[0]
+    }
+  },
+});
+
+
+function onClick (event) {
+event.preventDefault();
+
+const intervalId = setInterval(() => {
+  const currentDate = Date.now();
+  const countdownTimer = selectedDate.getTime() - currentDate;
+  if (countdownTimer <= 0) {
+    clearInterval(intervalId)
+}
+const { days, hours, minutes, seconds } = convertMs(countdownTimer);
+daysEl.textContent = `${days}`
+hoursEl.textContent = `${hours}`; 
+minutesEl.textContent = `${minutes}`; 
+secondsEl.textContent = `${seconds}`;
+    }, 1000);
+   }
+
+function pad (value){
+  return String(value).padStart(2,'0')
+};   
+
+function convertMs(ms) {
+const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = pad(Math.floor((ms % day) / hour));
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
+  }
+  
+ 
